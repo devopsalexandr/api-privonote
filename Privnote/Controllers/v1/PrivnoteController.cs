0721078d429e7333bd5d55560b4b2e9746ec7431
devopsalexandr <devopsalexandr@gmail.com>
2022-04-26 16:02:28 +0400

    simple string cryptor added

--- Privnote/Controllers/v1/PrivnoteController.cs
+++ Privnote/Controllers/v1/PrivnoteController.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using Privnote.DomainModel.Services.CryptService;
 
 namespace Privnote.Controllers.v1;
 
 public class PrivnoteController : ApiController
 {
-    
+    [HttpGet("/test")]
+    public string Test()
+    {
+        var stringCrypt = new StringCryptService();
+        var encryptString = stringCrypt.Encrypt("Hello message test", "123321");
+        
+        return stringCrypt.Decrypt(encryptString, "asdasd");
+    }
 }
